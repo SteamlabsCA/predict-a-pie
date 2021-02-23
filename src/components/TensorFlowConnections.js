4eaefea0d83b748c7d0c3569306d9823b3722681
Chris Maissan <chris@maissan.net>
2021-02-22 22:45:00 -0500

    Fixed connections glitch

--- src/components/TensorFlowConnections.js
+++ src/components/TensorFlowConnections.js
@@ -7,16 +7,12 @@ function TensorFlowConnections({inputs, layers, outputs}) {
   const svg = React.useRef();
 
   const neuronX = (neuron) => {
-    if (neuron.ref.current) {
-      const offset = document.querySelector('.TensorFlowConnections').offsetLeft;
-      return neuron.ref.current.offsetLeft + neuron.ref.current.offsetWidth / 2 - offset;
-    }
+    const offset = document.querySelector('.TensorFlowConnections').offsetLeft;
+    return neuron.ref.current.offsetLeft + neuron.ref.current.offsetWidth / 2 - offset;
   }
 
   const neuronY = (neuron) => {
-    if (neuron.ref.current) {
-      return neuron.ref.current.offsetTop + neuron.ref.current.offsetHeight / 2;
-    }
+    return neuron.ref.current.offsetTop + neuron.ref.current.offsetHeight / 2;
   };
 
   React.useEffect(() => {
@@ -27,13 +23,15 @@ function TensorFlowConnections({inputs, layers, outputs}) {
     if (layers.length) {
       for (let i = 0; i < inputs.length; i++) {
         for (let j = 0; j < layers[0].length; j++) {
-          lines.push({
-            x1: 0,
-            y1: neuronY(inputs[i]),
-            x2: neuronX(layers[0][j]),
-            y2: neuronY(layers[0][j]),
-            active: inputs[i].active
-          });
+          if (inputs[i].ref.current && layers[0][j].ref.current) {
+            lines.push({
+              x1: 0,
+              y1: neuronY(inputs[i]),
+              x2: neuronX(layers[0][j]),
+              y2: neuronY(layers[0][j]),
+              active: inputs[i].active
+            });
+          }
         }
       }
     }
@@ -42,13 +40,15 @@ function TensorFlowConnections({inputs, layers, outputs}) {
     for (let i = 0; i < layers.length - 1; i++) {
       for (let j = 0; j < layers[i].length; j++) {
         for (let k = 0; k < layers[i + 1].length; k++) {
-          lines.push({
-            x1: neuronX(layers[i][j]),
-            y1: neuronY(layers[i][j]),
-            x2: neuronX(layers[i + 1][k]),
-            y2: neuronY(layers[i + 1][k]),
-            active: layers[i][j].output > 0.2
-          });
+          if (layers[i][j].ref.current && layers[i + 1][k].ref.current) {
+            lines.push({
+              x1: neuronX(layers[i][j]),
+              y1: neuronY(layers[i][j]),
+              x2: neuronX(layers[i + 1][k]),
+              y2: neuronY(layers[i + 1][k]),
+              active: layers[i][j].output > 0.2
+            });
+          }
         }
       }
     }
@@ -57,19 +57,21 @@ function TensorFlowConnections({inputs, layers, outputs}) {
     if (layers.length) {
       for (let i = 0; i < layers[layers.length - 1].length; i++) {
         for (let j = 0; j < outputs.length; j++) {
-          lines.push({
-            x1: neuronX(layers[layers.length - 1][i]),
-            y1: neuronY(layers[layers.length - 1][i]),
-            x2: document.querySelector('.TensorFlowConnections').offsetWidth,
-            y2: neuronY(outputs[j]),
-            active: layers[layers.length - 1][i].output > 0.2
-          });
+          if (layers[layers.length - 1][i].ref.current && outputs[j].ref.current) {
+            lines.push({
+              x1: neuronX(layers[layers.length - 1][i]),
+              y1: neuronY(layers[layers.length - 1][i]),
+              x2: document.querySelector('.TensorFlowConnections').offsetWidth,
+              y2: neuronY(outputs[j]),
+              active: layers[layers.length - 1][i].output > 0.2
+            });
+          }
         }
       }
     }
 
     setLines([...lines]);
-  }, [layers]);
+  }, [inputs, layers, outputs]);
 
   return (
     <div className="TensorFlowConnections">
