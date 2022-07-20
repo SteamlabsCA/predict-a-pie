873ae988871b2e0f6616fa62b34a32e07b1d6591
MahoMori <tr27e1104@gmail.com>
2022-07-20 14:55:34 -0700

    Add the input feature for Ticker

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -3,6 +3,7 @@ import React from "react";
 
 function Ticker({ x, y, value, ...props }) {
   let mouseX, mouseY, previousValue;
+  const [isInputClicked, setIsInputClicked] = React.useState(false);
 
   const onMouseDown = (event) => {
     props.onStartChange();
@@ -18,19 +19,22 @@ function Ticker({ x, y, value, ...props }) {
   };
 
   const onMouseMove = (event) => {
-    const deltaX = event.clientX - mouseX;
-    const deltaY = -event.clientY + mouseY;
-    const delta = Math.abs(deltaX) > Math.abs(deltaY) ? deltaX : deltaY;
-    let newValue = parseInt(value) + Math.round(delta / 3);
-    newValue = Math.max(1, newValue);
-    newValue = Math.min(100, newValue);
-    if (newValue != previousValue) {
-      previousValue = newValue;
-      props.onChange(newValue);
+    if (isInputClicked === false) {
+      const deltaX = event.clientX - mouseX;
+      const deltaY = -event.clientY + mouseY;
+      const delta = Math.abs(deltaX) > Math.abs(deltaY) ? deltaX : deltaY;
+      let newValue = parseInt(value) + Math.round(delta / 3);
+      newValue = Math.max(1, newValue);
+      newValue = Math.min(100, newValue);
+      if (newValue != previousValue) {
+        previousValue = newValue;
+        props.onChange(newValue);
+      }
     }
   };
 
   const onClickPlus = () => {
+    setIsInputClicked(false);
     let newValue = value + 1;
     newValue = Math.max(1, newValue);
     newValue = Math.min(100, newValue);
@@ -41,6 +45,7 @@ function Ticker({ x, y, value, ...props }) {
   };
 
   const onClickMinus = () => {
+    setIsInputClicked(false);
     let newValue = value - 1;
     newValue = Math.max(1, newValue);
     newValue = Math.min(100, newValue);
@@ -50,15 +55,32 @@ function Ticker({ x, y, value, ...props }) {
     }
   };
 
+  const onClick = (event) => {
+    setIsInputClicked(true);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.key === "Escape") {
+      setIsInputClicked(false);
+    }
+    if (event.key === "Enter") {
+      setIsInputClicked(false);
+      let newValue = event.target.value;
+      newValue = Math.max(1, newValue);
+      newValue = Math.min(100, newValue);
+      if (newValue != previousValue) {
+        previousValue = newValue;
+        props.onChange(newValue);
+      }
+    }
+  };
+
   return (
     <div
       className="Ticker"
       onMouseDown={onMouseDown}
       style={{ left: x + -15 + "px", top: y + "px" }}
     >
-      {/* <div className="Ticker-control">
-        <div className="Ticker-detail" style={{transform: 'rotate(' + (value * 3.6) + 'deg)'}}></div>
-      </div> */}
       <button className="Ticker-button">
         <div
           className="Ticker-triangle Ticker-triangle-up"
@@ -73,8 +95,17 @@ function Ticker({ x, y, value, ...props }) {
           {Array(100)
             .fill()
             .map((x, i) => (
-              <div className="Ticker-value" key={i}>
-                {i + 1}
+              <div className="Ticker-value" key={i} onClick={onClick}>
+                {isInputClicked ? (
+                  <input
+                    className="Ticker-value Ticker-input"
+                    defaultValue={i + 1}
+                    onKeyDown={onKeyDown}
+                    autoFocus={value === i + 1 ? true : false}
+                  />
+                ) : (
+                  `${i + 1}`
+                )}
               </div>
             ))}
         </div>
@@ -83,6 +114,9 @@ function Ticker({ x, y, value, ...props }) {
         <div
           className="Ticker-triangle Ticker-triangle-down"
           onClick={onClickMinus}
+          // onMouseDown={onMouseDownMinus}
+          // onMouseUp={onMouseUpMinus}
+          // onMouseLeave={onMouseUpMinus}
         ></div>
       </button>
     </div>
