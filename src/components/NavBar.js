71505e039f6cc5865c237d24ad43a884dc1c327f
MahoMori <tr27e1104@gmail.com>
2022-11-16 13:38:15 -0800

    Create a NavBar for NN pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import './NavBar.scss';
-import Nav from './Nav';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import "./NavBar.scss";
+// import Nav from "./Nav";
+import Nav from "./NewNav";
+import { useParams } from "react-router-dom";
 
 function NavBar({ title, content, appData, checkEnv, envVariables, ...props }) {
-	const isMountedRef = React.useRef(null);
-	let { id } = useParams();
+  const isMountedRef = React.useRef(null);
+  let { id } = useParams();
 
-	React.useEffect(() => {
-		isMountedRef.current = true;
-		if (isMountedRef.current && checkEnv) {
-			checkEnv();
-		}
-		return () => (isMountedRef.current = false);
-	}, []);
+  React.useEffect(() => {
+    isMountedRef.current = true;
+    if (isMountedRef.current && checkEnv) {
+      checkEnv();
+    }
+    return () => (isMountedRef.current = false);
+  }, []);
 
-	return (
-		<header className='NavBar'>
-			<Nav appData={appData} route={props.route} onCommand={props.onCommand} />
-			<h1>{title === 'customPretrain' ? `Test Chef ${id.charAt(0).toUpperCase() + id.slice(1)}'s Pretrained Network` : title}</h1>
-			{envVariables && content}
-		</header>
-	);
+  return (
+    <header className="NavBar">
+      <Nav appData={appData} route={props.route} onCommand={props.onCommand} />
+      {/* <h1>{title === 'customPretrain' ? `Test Chef ${id.charAt(0).toUpperCase() + id.slice(1)}'s Pretrained Network` : title}</h1> */}
+      {envVariables && content && (
+        <div className="NavBar-NN">
+          <h1>
+            {title === "customPretrain"
+              ? `Test Chef ${
+                  id.charAt(0).toUpperCase() + id.slice(1)
+                }'s Pretrained Network`
+              : title}
+          </h1>
+          <div>{content}</div>
+        </div>
+      )}
+    </header>
+  );
 }
 
 export default NavBar;
