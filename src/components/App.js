06a02113c202d70f597b35636078670696e1ad5c
MahoMori <tr27e1104@gmail.com>
2023-01-10 08:37:37 -0800

    Change socket client url

--- src/components/App.js
+++ src/components/App.js
@@ -39,10 +39,10 @@ let socket;
 //   socket = socketClient(`https://127.0.0.1/`);
 //   console.log("staging");
 // } else if (process.env.NODE_ENV === "production") {
-//   socket = socketClient(`https://${window.location.hostname}`);
+socket = socketClient(`https://${window.location.hostname}`);
 // }
 
-socket = socketClient(`https://127.0.0.1:443`);
+// socket = socketClient(`https://127.0.0.1:443`);
 
 // Classroom code specified in URL
 const url = window.location.pathname.split("/");
